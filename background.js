b89dc01041bb12e561f7237d35b28875765c1cf4
kirill-korolev <kafafyf@gmail.com>
2017-03-16 10:21:27 +0300

    initial commit

---
+++ background.js
@@ -0,0 +1,64 @@
+var isEnabled = true;
+
+var icons = {
+  disabled:"icon481.png",
+  active:"icon48.png"
+};
+
+sendMessage(isEnabled);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+  if(isEnabled){
+    isEnabled = false;
+    chrome.browserAction.setIcon({
+      path:icons.disabled
+    });
+  }
+  else{
+    chrome.browserAction.setIcon({
+      path:icons.active
+    });
+
+    isEnabled = true;
+  }
+
+  sendMessage(isEnabled);
+
+});
+
+
+function sendMessage(isEnabled){
+    var tabId;
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, {isEnabled: isEnabled});
+    });
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendRespons)
+{
+    document.body.innerHTML += request.data;
+
+    var svg = document.querySelector('svg');
+    svg.setAttribute('id', "icon_" + randomizeString());
+
+    var e = document.createElement('script');
+    e.setAttribute('src', 'https://nytimes.github.io/svg-crowbar/svg-crowbar.js');
+    e.setAttribute('class', 'svg-crowbar');
+    document.body.appendChild(e);
+});
+
+function randomizeString(){
+    var result = "";
+
+    for(i = 0; i < 6; i++){
+        result += getRandom(0, 9).toString();
+    }
+
+    return result;
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min+1) + min);
+}
---
+++ content_script.js
@@ -0,0 +1,26 @@
+var isEnabled = true;
+var svg = document.querySelector('.icon-preview__svg');
+
+svg.addEventListener("click", function(){
+
+    if(isEnabled){
+        var data = svg.innerHTML;
+        chrome.runtime.sendMessage({data: data});
+    }
+
+});
+
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if(message.isEnabled){
+      isEnabled = true;
+    }
+    else{
+      isEnabled = false;
+    }
+});
+
+function Object(path, fill){
+    this.path = path;
+    this.fill = fill;
+}
